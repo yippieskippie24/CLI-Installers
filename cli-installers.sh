ed5f505e7982a52ee7fe37b641759e4600d19b14
Tyler M Johnson <yippieskippie24@gmail.com>
2019-12-04 19:25:25 -0600

    Created cli-installers.sh and variables-and-functions files

---
+++ cli-installers.sh
@@ -0,0 +1,28 @@
+#!/bin.bash
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#                                                         #
+#                  CLI installer Script                   #
+#                                                         #
+#        This script allows a user to selectively         #
+#       install custom scripts and other programs.        #
+#                                                         #
+#         This script is written specifically for         #
+#                    Ubuntu 18.04 LTS.                    #
+#                                                         #
+#              Written by:  Tyler M Johnson               #
+#           https://github.com/yippieskippie24            #
+#                                                         #
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#                     Source files                        #
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+source variables-and-functions
+
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#                    Calling Functions                    #
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
