a8de71832ae515fb5857a3ec5b05f77f27e2a6e4
Tyler M Johnson <yippieskippie24@gmail.com>
2019-12-05 20:02:58 -0600

    If required files are not present they'll download from github

--- cli-installers.sh
+++ cli-installers.sh
@@ -15,6 +15,24 @@
 #                                                         #
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#                       File Check                        #
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+#Saves the directory that the ./cli-installer.sh was run from to $DIR
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#verify the variables-and-functions file exists.  If not, download it from github.
+if [ ! -d "$DIR/variables-and-functions" ]; then
+  curl -o variables-and-functions https://raw.githubusercontent.com/yippieskippie24/CLI-Installers/master/variables-and-functions
+fi
+
+#verify the software file exists.  If not, download it from github.
+if [ ! -d "$DIR/software" ]; then
+  curl -o software https://raw.githubusercontent.com/yippieskippie24/CLI-Installers/master/software
+fi
+
+
 
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 #                     Source files                        #
